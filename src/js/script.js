fe187143624cb0c3c2ed7b72d01746a9902123fb
uldalex <uldalex@gmail.com>
2021-02-19 10:20:49 +0300

    каммит от 18 февраля. Основные страницы+ адаптивка

--- src/js/script.js
+++ src/js/script.js
@@ -931,26 +931,11 @@ $(audioButton).on('click', function() {
  $('svg.wave').toggleClass('played');
  
  });
+ $('.img').on('click', function() {
+	$(this).toggleClass('scale');
 
-$(document).ready(function() {
-  var playing = false;
-
-  $('a#button').click(function() {
-      $(this).toggleClass("down");
-
-      if (playing == false) {
-          document.getElementById('player').play();
-          playing = true;
-          $(this).text("stop sound");
-
-      } else {
-        document.getElementById('player').pause();
-        playing = false;
-        $(this).text("restart sound");
-      }
-
-  });
-});
+	
+	});
 
 
   var wow = new WOW(
