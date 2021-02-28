d5d304b397e55fa145c1f02b216c9cdd7e59922a
uldalex <uldalex@gmail.com>
2021-02-28 17:05:07 +0300

    большой каммит

--- node_modules1/baron/src/client.js
+++
@@ -1,7 +0,0 @@
-var _baron = require('./core')
-
-window.baron = _baron
-
-if (window.jQuery && window.jQuery.fn) {
-    window.jQuery.fn.baron = _baron
-}
--- node_modules1/baron/src/log.js
+++
@@ -1,9 +0,0 @@
-module.exports = function log(level, msg, more) {
-    var func = console[level] || console.log
-    var args = [
-        'Baron: ' + msg,
-        more
-    ]
-
-    Function.prototype.apply.call(func, console, args)
-}
